7cf30211da4cddecbe4216cf34c171734b29d624
Jonas M. Miguel <jonasmmiguel@gmail.com>
2022-08-29 14:03:45 +0000

    [setup] added initial structure

---
+++ hello.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+
+def main():  # pragma: no cover
+    x = 1
+    y = 2
+    print(f"summing {x} and {y}: {add(x,y)}")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
